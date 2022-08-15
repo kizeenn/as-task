b2fbdeded58dec7b0955f3c36e0c797934bfcb4a
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-08-15 22:29:24 +0200

    Display events

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
-import "./App.css";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+  Grid,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { Event } from "./mocks/handlers";
 
 function App() {
-  return <div className="App"></div>;
+  const [getEvents, setEvents] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch("/api/events");
+    const data = await response.json();
+    setEvents(data.events);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={10}
+    >
+      {getEvents.map((event: Event) => (
+        <Grid item>
+          <Card sx={{ maxWidth: 500 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={event.image}
+                alt="green iguana"
+              />
+
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {event.title}
+                </Typography>
+
+                <Typography variant="body2" color="text.secondary">
+                  {event.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
 if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
