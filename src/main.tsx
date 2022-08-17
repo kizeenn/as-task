9921a0fc42b1700617dfd6841dffed3b2f0a81f3
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-08-17 14:33:18 +0200

    Install and setup tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
 if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
