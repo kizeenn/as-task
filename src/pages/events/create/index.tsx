6bfb29efbba52997c3fa70cb36f1b7891cf81809
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-08-18 19:09:37 +0200

    Add clear form button

--- src/pages/events/create/index.tsx
+++ src/pages/events/create/index.tsx
@@ -151,12 +151,21 @@ function CreateEventIndexPage() {
               value="phoneNumber"
             />
 
+            <div className="flex justify-end">
+              <button
+                type="reset"
+                className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Clear
+              </button>
+
               <button
                 type="submit"
                 className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Save
               </button>
+            </div>
           </Form>
         </div>
       )}
