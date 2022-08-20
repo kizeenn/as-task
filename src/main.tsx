41ba326ba4493d7271233ddd18d27d8f7a4f349c
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-08-20 13:15:44 +0200

    Fix vercel deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 
-if (import.meta.env.DEV) {
-  const { worker } = await import("./mocks/browser");
-  worker.start({ onUnhandledRequest: "bypass" });
-}
+
+const { worker } = await import("./mocks/browser");
+worker.start({ onUnhandledRequest: "bypass" });
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
